2763796ed0b0a0583408e533295e5e03d44f6e2e
a-moseman <drewmoseman@gmail.com>
2024-04-13 15:49:25 -0400

    Initialize spring boot project files

---
+++ src/main/java/org/letheproject/lethecore/LetheCoreApplication.java
@@ -0,0 +1,13 @@
+package org.letheproject.lethecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetheCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetheCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/letheproject/lethecore/LetheCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.letheproject.lethecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetheCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
