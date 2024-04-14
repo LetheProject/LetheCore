faa5922336cdf36647f152a897d4eb020fee4c5d
a-moseman <drewmoseman@gmail.com>
2024-04-14 11:29:35 -0400

    Implement class for Argon2ID

---
+++ src/main/java/org/letheproject/lethecore/cryptography/hashing/Argon2ID.java
@@ -0,0 +1,39 @@
+package org.letheproject.lethecore.cryptography.hashing;
+
+import org.bouncycastle.crypto.generators.Argon2BytesGenerator;
+import org.bouncycastle.crypto.params.Argon2Parameters;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+
+import java.nio.charset.StandardCharsets;
+
+public class Argon2ID extends Hasher {
+    private static final int HASH_LENGTH = 64;
+    private static final int PARALLELISM_FACTOR = 1;
+    private static final int MEMORY_COST = 60_000;
+    private static final int ITERATIONS = 10;
+    private final Argon2BytesGenerator generator;
+
+    public Argon2ID() {
+        this.generator = new Argon2BytesGenerator();
+    }
+
+    @Override
+    public byte[] hash(byte[] data, byte[] salt) {
+        generator.init(new Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
+                .withVersion(Argon2Parameters.ARGON2_VERSION_13)
+                .withIterations(ITERATIONS)
+                .withMemoryAsKB(MEMORY_COST)
+                .withParallelism(PARALLELISM_FACTOR)
+                .withSalt(salt)
+                .build()
+        );
+        return hash(data);
+    }
+
+    @Override
+    public byte[] hash(byte[] data) {
+        byte[] hash = new byte[HASH_LENGTH];
+        generator.generateBytes(data, hash, 0, HASH_LENGTH);
+        return hash;
+    }
+}
---
+++ src/test/java/org/letheproject/lethecore/cryptography/hashing/Argon2IDTest.java
@@ -0,0 +1,39 @@
+package org.letheproject.lethecore.cryptography.hashing;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Argon2IDTest {
+
+    @Test
+    void hash() {
+        Argon2ID argon2ID = new Argon2ID();
+        byte[] data = "Hello World!".getBytes(StandardCharsets.UTF_8);
+        byte[] saltA = new byte[]{0, 0, 0, 1};
+        byte[] saltB = new byte[]{0, 0, 0, 2};
+        byte[] saltC = new byte[]{0, 0, 0, 1};
+        byte[] hashA = argon2ID.hash(data, saltA);
+        byte[] hashB = argon2ID.hash(data, saltB);
+        byte[] hashC = argon2ID.hash(data, saltC);
+        boolean condition = true;
+        for (int i = 0; i < hashA.length; i++) {
+            if (hashA[i] != hashB[i]) {
+                condition = false;
+                break;
+            }
+        }
+        assertFalse(condition);
+        condition = true;
+        for (int i = 0; i < hashA.length; i++) {
+            if (hashA[i] != hashC[i]) {
+                condition = false;
+                break;
+            }
+        }
+        assertTrue(condition);
+    }
+}
