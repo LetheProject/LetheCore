842d7958a00f437bd3cefec0e9bd3dedbe4f620d
a-moseman <drewmoseman@gmail.com>
2024-04-15 08:46:40 -0400

    Add javadoc

--- src/main/java/org/letheproject/lethecore/dataprocessing/DataProcessor.java
+++ src/main/java/org/letheproject/lethecore/dataprocessing/DataProcessor.java
@@ -7,28 +7,51 @@ import org.letheproject.lethecore.cryptography.encryption.Encryptor;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Defines a sequence of data processing events, namely encryption and compression.
+ * Provides for the execution of the sequence and its inverse.
+ */
 public class DataProcessor {
     private final List<Encryptor> encryptors;
     private final List<byte[]> encryptorKeys;
     private final List<Compressor> compressors;
 
+    /**
+     * Instantiate a DataProcessor.
+     */
     public DataProcessor() {
         this.encryptors = new ArrayList<>();
         this.encryptorKeys = new ArrayList<>();
         this.compressors = new ArrayList<>();
     }
 
+    /**
+     * Add an encryptor to the sequence.
+     * @param encryptor the encryptor to add.
+     * @param key the key to use with the encryptor.
+     * @return this.
+     */
     public DataProcessor addEncryptor(Encryptor encryptor, byte[] key) {
         encryptors.add(encryptor);
         encryptorKeys.add(key);
         return this;
     }
 
+    /**
+     * Add a compressor to the sequence.
+     * @param compressor the compressor to add.
+     * @return this.
+     */
     public DataProcessor addCompressor(Compressor compressor) {
         compressors.add(compressor);
         return this;
     }
 
+    /**
+     * Execute the sequence forward.
+     * @param in the data to process.
+     * @return the processed data.
+     */
     public byte[] forward(byte[] in) {
         byte[] out = Arrays.clone(in);
         for (int i = 0; i < encryptors.size(); i++) {
@@ -42,6 +65,11 @@ public class DataProcessor {
         return out;
     }
 
+    /**
+     * Execute the sequence backward.
+     * @param in the data to process.
+     * @return the processed data.
+     */
     public byte[] backward(byte[] in) {
         byte[] out = Arrays.clone(in);
         for (int i = compressors.size() - 1; i >= 0; i--) {
