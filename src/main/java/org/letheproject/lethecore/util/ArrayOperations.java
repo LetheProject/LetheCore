3fd8bd6162586d518e483eb8e71088bcd3915257
a-moseman <drewmoseman@gmail.com>
2024-04-15 08:57:28 -0400

    Add javadoc

--- src/main/java/org/letheproject/lethecore/util/ArrayOperations.java
+++ src/main/java/org/letheproject/lethecore/util/ArrayOperations.java
@@ -1,6 +1,13 @@
 package org.letheproject.lethecore.util;
 
 public class ArrayOperations {
+    /**
+     * Extract a sub-array from the byte array.
+     * @param data the byte array.
+     * @param start the starting index (inclusive) of the sub-array.
+     * @param end the ending index (exclusive) of the sub-array.
+     * @return the sub-array.
+     */
     public static byte[] extract(byte[] data, int start, int end) {
         int length = end - start;
         byte[] out = new byte[length];
@@ -8,6 +15,12 @@ public class ArrayOperations {
         return out;
     }
 
+    /**
+     * Concatenate two byte arrays.
+     * @param a the first byte array.
+     * @param b the second byte array.
+     * @return the concatenation of a and b.
+     */
     public static byte[] concatenate(byte[] a, byte[] b) {
         byte[] c = new byte[a.length + b.length];
         System.arraycopy(a, 0, c, 0, a.length);
