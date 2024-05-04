12f810cd3949f4f43a74336c3461ab1789823d9a
a-moseman <drewmoseman@gmail.com>
2024-05-03 23:16:22 -0400

    Make ShardFactory.build expect an Encoder and a Hasher, in addition to adding JavaDoc

--- src/main/java/org/letheproject/lethecore/node/shard/ShardFactory.java
+++ src/main/java/org/letheproject/lethecore/node/shard/ShardFactory.java
@@ -2,18 +2,22 @@ package org.letheproject.lethecore.node.shard;
 
 import org.bouncycastle.openpgp.PGPException;
 import org.bouncycastle.openpgp.PGPKeyPair;
+import org.letheproject.lethecore.cryptography.encoding.Encoder;
 import org.letheproject.lethecore.cryptography.encoding.Hexadecimal;
+import org.letheproject.lethecore.cryptography.hashing.Hasher;
 import org.letheproject.lethecore.cryptography.hashing.SHA256;
 import org.letheproject.lethecore.cryptography.keys.PGPKeyService;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * A factory for the Shard class.
+ */
 public class ShardFactory {
     private byte[] contents;
     private byte[] name;
     private byte[] virtualFilePath;
     private byte[] positionInOriginFile;
-
     private String ownerIdentity;
     private long duplicationID;
     private long created;
@@ -72,21 +76,27 @@ public class ShardFactory {
         return this;
     }
 
-    public Shard build(PGPKeyService keyService, PGPKeyPair keyPair) {
-        Hexadecimal hex = new Hexadecimal();
-        SHA256 sha256 = new SHA256();
+    /**
+     * Build a shard from the set arguments.
+     * @param encoder the encoder to use.
+     * @param hasher the hasher to use.
+     * @param keyService the key service to use for signing the shard.
+     * @param keyPair the key pair to use in signing the shard.
+     * @return the shard.
+     */
+    public Shard build(Encoder encoder, Hasher hasher, PGPKeyService keyService, PGPKeyPair keyPair) {
         String concatenated = String.format(
                 "{%s, %s, %s, %s, %s, %s, %s, %s, %s, %s}",
-                hex.encode(new String(name)),
-                hex.encode(new String(contents)),
-                hex.encode(new String(virtualFilePath)),
-                hex.encode(new String(positionInOriginFile)),
-                hex.encode(ownerIdentity),
-                hex.encode(fileUUID),
-                hex.encode(String.valueOf(shardID)),
-                hex.encode(String.valueOf(duplicationID)),
-                hex.encode(String.valueOf(created)),
-                hex.encode(transformationUUID)
+                encoder.encode(new String(name)),
+                encoder.encode(new String(contents)),
+                encoder.encode(new String(virtualFilePath)),
+                encoder.encode(new String(positionInOriginFile)),
+                encoder.encode(ownerIdentity),
+                encoder.encode(fileUUID),
+                encoder.encode(String.valueOf(shardID)),
+                encoder.encode(String.valueOf(duplicationID)),
+                encoder.encode(String.valueOf(created)),
+                encoder.encode(transformationUUID)
         );
         byte[] signature;
         try {
@@ -95,8 +105,8 @@ public class ShardFactory {
         catch (PGPException e) {
             throw new RuntimeException(e);
         }
-        concatenated += "\n" + hex.encode(new String(signature));
-        byte[] checksum = sha256.hash(concatenated.getBytes(StandardCharsets.UTF_8));
+        concatenated += "\n" + encoder.encode(new String(signature));
+        byte[] checksum = hasher.hash(concatenated.getBytes(StandardCharsets.UTF_8));
         ShardMetadata metadata = new ShardMetadata(fileUUID, shardID, transformationUUID, checksum);
         ShardPublicData publicData = new ShardPublicData(metadata, ownerIdentity, duplicationID, created, signature);
         return new Shard(publicData, contents, name, virtualFilePath, positionInOriginFile);
