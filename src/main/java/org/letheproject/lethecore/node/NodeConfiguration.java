6773dad769d13f5f9684d9fb2af3b995ed7da384
a-moseman <drewmoseman@gmail.com>
2024-05-04 13:09:13 -0400

    Delete NodeConfiguration

--- src/main/java/org/letheproject/lethecore/node/NodeConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.letheproject.lethecore.node;
-
-public record NodeConfiguration(
-        long shardSize,
-        long bandwidthPeriodHours,
-        long bandwidthPerPeriod,
-        long shardStorageQuota,
-        String duplicationFormula
-){}
