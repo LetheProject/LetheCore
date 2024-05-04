82af4d59de9cfc466a9e40d7fdc6989e61173d4d
a-moseman <drewmoseman@gmail.com>
2024-05-04 10:37:39 -0400

    Add definition for getting the shortest path

--- src/main/java/org/letheproject/lethecore/node/TrustDAO.java
+++ src/main/java/org/letheproject/lethecore/node/TrustDAO.java
@@ -3,5 +3,16 @@ package org.letheproject.lethecore.node;
 import java.util.List;
 
 public interface TrustDAO {
+    /**
+     * Get the list of trusted identities, both direct and indirect.
+     * @return the list of identities.
+     */
     List<TrustedNodeIdentity> getTrustedIdentities();
+
+    /**
+     * Get the shortest path, in terms of identities, from this node to the target node.
+     * @param targetIdentity the identity of the target node.
+     * @return the shortest path.
+     */
+    List<String> shortestIdentityPath(String targetIdentity);
 }
