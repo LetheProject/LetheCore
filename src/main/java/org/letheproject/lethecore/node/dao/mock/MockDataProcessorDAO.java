488eef5dbddd9ac6e67f0f7e085a17b581aed585
a-moseman <drewmoseman@gmail.com>
2024-05-04 13:13:23 -0400

    Implement a mock DataProcessorDAO

--- src/main/java/org/letheproject/lethecore/node/dao/mock/MockDataProcessorDAO.java
+++ src/main/java/org/letheproject/lethecore/node/dao/mock/MockDataProcessorDAO.java
@@ -3,28 +3,32 @@ package org.letheproject.lethecore.node.dao.mock;
 import org.letheproject.lethecore.dataprocessing.DataProcessor;
 import org.letheproject.lethecore.node.dao.DataProcessorDAO;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class MockDataProcessorDAO implements DataProcessorDAO {
     private HashMap<String, DataProcessor> processors;
 
     public MockDataProcessorDAO() {
-
+        processors = new HashMap<>();
     }
 
     @Override
     public DataProcessor getDataProcessor(String uuid) {
-        return null;
+        return processors.get(uuid);
     }
 
     @Override
     public String addDataProcessor(DataProcessor dataProcessor) {
-        return null;
+        String uuid = UUID.randomUUID().toString();
+        processors.put(uuid, dataProcessor);
+        return uuid;
     }
 
     @Override
     public List<String> listUUIDs() {
-        return null;
+        return new ArrayList<>(processors.keySet());
     }
 }
